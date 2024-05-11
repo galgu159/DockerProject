4227711a28a17fd929c942e75ca87012dd59831a
galg <galguet@gmail.com>
2024-05-11 16:45:29 +0300

    test

--- mongo_init/mongo_init.sh
+++ mongo_init/mongo_init.sh
@@ -1,29 +1,22 @@
 #!/bin/bash
 
-echo "Initializing MongoDB replica set..."
+# Function to initialize the replica set
+#
 
-# Start MongoDB server with replica set configuration
-mongod --replSet myReplicaSet --bind_ip localhost,mongo_primary &
-
-# Wait for MongoDB to start
 sleep 10
-
-echo "Initializing MongoDB replica set..."
-mongo --host mongo_primary:27017 <<EOF
-rs.initiate({
-  _id: "myReplicaSet",
-  version: 1,
-  members: [
-    { _id: 0, host: "mongo_primary:27017" },
-    { _id: 1, host: "mongo_1:27017" },
-    { _id: 2, host: "mongo_2:27017" }
-  ]
-})
+initialize_replica_set() {
+  mongo --host "$1" <<EOF
+  rs.initiate({
+    _id: "myReplicaSet",
+    version: 1,
+    members: [
+      { _id: 0, host: "mongo_1:27017", priority: 2 },
+      { _id: 1, host: "mongo_2:27017", priority: 1 },
+      { _id: 2, host: "mongo_3:27017", priority: 1 }
+    ]
+  })
 EOF
+}
 
-# Check if replica set initialization was successful
-if [ $? -eq 0 ]; then
-  echo "MongoDB replica set initialized successfully."
-else
-  echo "Error: MongoDB replica set initialization failed."
-fi
+# Initialize the replica set on mongo_1:27017
+initialize_replica_set "mongo_1:27017"
