c9e9ec919e1c38f3af7cc358267a6b0237f34c05
galg <galguet@gmail.com>
2024-05-17 20:43:06 +0300

    add TELEGRAM_APP_URL var

--- polybot/app.py
+++ polybot/app.py
@@ -7,16 +7,15 @@ from bot import Bot, ObjectDetectionBot
 app = flask.Flask(__name__)
 
 
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-TELEGRAM_APP_URL = os.getenv('TELEGRAM_APP_URL')
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TELEGRAM_APP_URL = os.environ['TELEGRAM_APP_URL']
+
 
 @app.route('/', methods=['GET'])
-
 def index():
     return 'Ok'
 
 @app.route(f'/{TELEGRAM_TOKEN}/', methods=['POST'])
-
 def webhook():
     req = request.get_json()
     bot.handle_message(req['message'])
